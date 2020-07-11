e8c1e4d75439ecda483f497eee0ab8a2d53fd8dd
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-11 12:16:12 -0600

    added merchant controller and items controller for API

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,4 +1,4 @@
-class API::V1::ItemsController < ApplicationController
+class Api::V1::ItemsController < ApplicationController
   def index
     render json: Item.all
   end
@@ -15,9 +15,13 @@ class API::V1::ItemsController < ApplicationController
     render json: Item.update(params[:id], item_params )
   end
 
+  def destroy
+    render json: Item.delete(params[:id])
+  end
+
   private
 
   def item_params
-    params.permit(:name, :description, :unit_price, :merchant_id)
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
   end
 end
