a723556e79f409338b2175eef5959bfaa4f6e656
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-11 16:02:00 -0600

    Add tests for all models including vlaidations and relationships

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Invoice, type: :model do
+  describe 'Relationships' do
+    it {should belong_to :customer}
+    it {should belong_to :merchant}
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of(:status)}
+  end
+
+end
