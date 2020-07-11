0b603ad4ec620985c74f73bf160570f97c030674
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-11 16:00:37 -0600

    add factory for creating a new item

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { "Chew Toy" }
     description { "Best chew toy!" }
-    unit_price {1111}
+    unit_price {11.11}
     merchant_id {create(:merchant).id}
   end
 end
