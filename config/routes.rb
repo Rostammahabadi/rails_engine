50624184244a3c0f06c749a6460e4c03c246c1c4
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-11 16:00:27 -0600

    Add route for finding a resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
       resources :merchants
       resources :invoices
       resources :transactions
+      get "/merchants/:id/items", to: 'merchant_items#index'
+      get "/items/:id/merchant", to: 'merchant_item#show'
+      get "/:resource/find", to: 'search#show'
     end
   end
 end
