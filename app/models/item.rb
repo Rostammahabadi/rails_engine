ed68cc6404ff27e3eb2b02e7033fd6b7cfd96fa0
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-11 22:23:13 -0600

    added class method to search by params

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   validates_presence_of :name, :description, :unit_price
   belongs_to :merchant
+
+  def self.find_by_params(params)
+    if params[:name]
+      Item.where('lower(name) LIKE ?', "%#{params[:name].downcase}%")
+    elsif params[:description]
+      Item.wherd('lower(description) LIKE ?', "%#{params[:description].downcase}%")
+    elsif params[:unit_price]
+      Item.where('unit_price LIKE ?', "%#{parms[:unit_price]}%")
+    elsif params[:merchant_id]
+
+    elsif params[:created_at]
+
+    else
 end
