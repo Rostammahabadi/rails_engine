4014e83d74f06335c8039735167ec667148cb038
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-11 22:23:24 -0600

    added class method to search by params

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,14 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
+
+  def self.find_by_params(params)
+    if params[:name]
+      Merchant.where('lower(name) LIKE ?', "%#{params[:name].downcase}%")
+    elsif params[:created_at]
+      Merchant.where('created_at LIKE ?', "%#{params[:created_at]}%")
+    elsif params[:updated_at]
+      Merchant.where('updated_at LIKE ?', "%#{params[:updated_at]}%")
+    end
+  end
 end
