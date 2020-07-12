79eb705b746214e738a7dedaffab1dc39dbdb253
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-11 22:24:12 -0600

    wrote minimal test for find controller which returns object as expected

--- spec/requests/api/v1/find_endpoints/find_request_spec.rb
+++ spec/requests/api/v1/find_endpoints/find_request_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 describe "User can find a resource" do
   it "returns the json for the attributes" do
+    merchant = create(:merchant)
+    get "/api/v1/merchants/find?name=dog"
 
-    get "/api/v1/merchant/find?name=dog"
-
-    binding.pry
+    expect(response).to be_successful
+    merchant_response = JSON.parse(response.body)
+    expect(merchant_response.count).to eq(1)
   end
 end
