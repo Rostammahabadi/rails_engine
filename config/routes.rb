e11420f24b5e43f07bf6abd274497477619c431b
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-11 22:23:50 -0600

    added route to search merchant and item for params which are namespaced under the corresponding return object

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#index'
+      end
+      namespace :items do
+        get '/find', to: 'search#index'
+      end
       resources :items
       resources :customers
       resources :invoice_items
-      resources :merchants
       resources :invoices
       resources :transactions
+      resources :merchants
       get "/merchants/:id/items", to: 'merchant_items#index'
       get "/items/:id/merchant", to: 'merchant_item#show'
       get "/:resource/find", to: 'search#show'
