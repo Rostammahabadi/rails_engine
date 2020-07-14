6d2428814819fbe0abcf8b64654b1e96b1d767c8
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-13 20:46:01 -0600

    added method which searches for a single record and multiple

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::Items::SearchController < ApplicationController
+  def show
+    render json: ItemSerializer.new(Item.find_by_params(items_params))
+  end
+
   def index
-    render json: Item.find_by_params(params)
+    render json: ItemSerializer.new(Item.find_all_by_params(items_params))
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
   end
 end
