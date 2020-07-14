dde4e8fbe23e34becf7654c3a73ed748b41183f0
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-13 22:27:34 -0600

    added methods for find and find_all

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,16 @@ class Merchant < ApplicationRecord
 
   def self.find_by_params(params)
     if params[:name]
+      Merchant.where('lower(name) LIKE ?', "%#{params[:name].downcase}%").first
+    elsif params[:created_at]
+      Merchant.where('created_at LIKE ?', "%#{params[:created_at]}%").first
+    elsif params[:updated_at]
+      Merchant.where('updated_at LIKE ?', "%#{params[:updated_at]}%").first
+    end
+  end
+
+  def self.find_all_by_params(params)
+    if params[:name]
       Merchant.where('lower(name) LIKE ?', "%#{params[:name].downcase}%")
     elsif params[:created_at]
       Merchant.where('created_at LIKE ?', "%#{params[:created_at]}%")
