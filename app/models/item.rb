a0cf031fd8ff7547945007ccaf20052729aa9490
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-13 22:27:10 -0600

    updated update and destroy methods as well as not requiring the params

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,41 @@
 class Item < ApplicationRecord
-  validates_presence_of :name, :description, :unit_price
+  validates :name, presence: true, on: :create
+  validates :description, presence: true, on: :create
+  validates :unit_price, presence: true, on: :create
   belongs_to :merchant
 
   def self.find_by_params(params)
     if params[:name]
-      Item.where('lower(name) LIKE ?', "%#{params[:name].downcase}%")
+      Item.where('lower(name) LIKE ?', "%#{params[:name].downcase}%").first
     elsif params[:description]
-      Item.wherd('lower(description) LIKE ?', "%#{params[:description].downcase}%")
-    elsif params[:unit_price]
-      Item.where('unit_price LIKE ?', "%#{parms[:unit_price]}%")
-    elsif params[:merchant_id]
-      Item.where('merchant_id = ?', params[:merchant_id])
+      Item.where('lower(description) LIKE ?', "%#{params[:description].downcase}%").first
     elsif params[:created_at]
-      Item.where('created_at = ?', params[:created_at])
+      Item.where(created_at: "%#{params[:created_at]}%").first
     else
-      Item.where('updated_at = ?', params[:updated_at])
+      Item.where(updated_at: "%#{params[:updated_at]}%").first
+    end
+  end
+
+  # def self.find_all_by_params(params)
+  #   params.to_h.each do |key,value|
+  #     if key == 'name' || key == 'description'
+  #       Item.where("lower(#{key.downcase}) LIKE ?" ,"%#{value.downcase}%")
+  #     elsif key == 'created_at' || key == 'updated_at'
+  #       Item.where("#{key} = #{value}")
+  #     else
+  #       Item.where(merchant_id: value)
+  #     end
+  #   end
+  # end
+  def self.find_all_by_params(params)
+    if params[:name]
+      Item.where('lower(name) LIKE ?', "%#{params[:name]}%")
+    elsif params[:description]
+      Item.where('lower(description) LIKE ?', "%#{params[:description]}%")
+    elsif params[:created_at]
+      Item.where(created_at: "%#{params[:created_at]}%")
+    else
+      Item.where(updated_at: "%#{params[:updated_at]}%")
     end
   end
 end
