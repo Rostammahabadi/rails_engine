39185a1880308995921a341e5fdd1940da78d4a4
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-13 22:27:43 -0600

    added routes for search controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        get '/find', to: 'search#index'
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       namespace :items do
-        get '/find', to: 'search#index'
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       resources :items
       resources :customers
