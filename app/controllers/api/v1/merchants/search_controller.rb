80650e9ccbebac2c72ff6e9b4ce4013647f26ab8
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-13 20:46:14 -0600

    added method which searches for a single record and multiple

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Merchants::SearchController < ApplicationController
+  def show
+    render json: MerchantSerializer.new(Merchant.find_by_params(params))
+  end
+
   def index
-    render json: Merchant.find_by_params(params)
+    render json: MerchantSerializer.new(Merchant.find_all_by_params(params))
   end
 end
