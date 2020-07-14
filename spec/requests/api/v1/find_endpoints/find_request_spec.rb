4c21f8cab7f77a5317f312daec0409e4cdb26d72
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-13 22:28:16 -0600

    update find_Request

--- spec/requests/api/v1/find_endpoints/find_request_spec.rb
+++ spec/requests/api/v1/find_endpoints/find_request_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe "User can find a resource" do
   it "returns the json for the attributes" do
     merchant = create(:merchant)
-    get "/api/v1/merchants/find?name=dog"
+    item = create(:item)
+    get "/api/v1/items/find?name=dog&description=toy"
 
     expect(response).to be_successful
     merchant_response = JSON.parse(response.body)
+    binding.pry 
     expect(merchant_response.count).to eq(1)
   end
 end
