1a51895e3db014906159f9637736805134f34b07
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-15 20:48:57 -0600

    added methods for most items, revenue between dates, and merchant revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,8 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
 
   def self.find_by_params(params)
     if params[:name]
@@ -20,5 +22,35 @@ class Merchant < ApplicationRecord
     elsif params[:updated_at]
       Merchant.where('updated_at LIKE ?', "%#{params[:updated_at]}%")
     end
+  end
+
+  def self.most_revenue(params)
+    select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue').
+    joins(invoices: [:transactions, :invoice_items]).
+    where(transactions: {result: 'success'}).
+    group('merchants.id').order('total_revenue DESC').
+    limit("#{params[:quantity].to_i}")
+  end
+
+  def self.most_items(params)
+    select('merchants.*, sum(invoice_items.quantity) AS total_quantity').
+    joins(invoices: [:transactions, :invoice_items]).
+    where(transactions: {result: 'success'}).
+    group('merchants.id').
+    order('total_quantity DESC').
+    limit("#{params.to_i}")
+  end
+
+  def self.revenue_between_dates(params)
+    select('sum(invoice_items.quantity * invoice_items.unit_price) AS revenue').
+    joins(invoices: [:transactions, :invoice_items]).
+    where(transactions: {result: 'success'}).
+    where("to_char(invoices.updated_at, 'YYYY-MM-DD') BETWEEN '#{params[:start]}' AND '#{params[:end]}'")
+  end
+
+  def self.merchant_revenue
+    sql_injection = "SELECT  max(m_revenue) as revenue FROM (SELECT merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as m_revenue FROM merchants INNER JOIN invoices on merchants.id = invoices.merchant_id INNER JOIN invoice_items ON invoices.id = invoice_items.invoice_id INNER JOIN transactions ON invoices.id = transactions.invoice_id WHERE transactions.result = 'success' GROUP BY merchants.id) AS revenue"
+    find_by_sql(sql_injection)
+    # select('max(revenue)').from(select('sum(invoice_items.quantity * invoice_items.unit_price) AS revenue').joins(invoices: [:transactions, :invoice_items]).where(transactions: {result: 'success'}))
   end
 end
