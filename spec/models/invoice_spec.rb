c4cfa34efacf2e52b08b992c2c558ae9d58578a6
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-15 23:17:52 -0600

    add tests for models

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Invoice, type: :model do
   describe 'Relationships' do
     it {should belong_to :customer}
     it {should belong_to :merchant}
+    it {should have_many :invoice_items}
+    it {should have_many :transactions}
   end
 
   describe 'Validations' do
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,5 +7,20 @@ RSpec.describe Merchant, type: :model do
 
   describe 'Relationships' do
     it {should have_many(:items)}
+    it {should have_many(:invoices)}
+    it {should have_many(:invoice_items).through(:invoices)}
+  end
+
+  describe 'Class Methods' do
+    it 'gets the merchant by name' do
+      merchant = create(:merchant)
+      response = Merchant.find_by_params(name: "dog")
+      expect(response.id).to eq(merchant.id)
+    end
+    it 'gets the merchant by created_at' do
+      merchant = create(:merchant, created_at: Date.new(2012-01-01))
+      response = Merchant.find_by_params(created_at: "2010")
+      expect(response.id).to eq(merchant.id)
+    end
   end
 end
